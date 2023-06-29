c2dc8ba6d1c9250b83c26e68de1370988fd3f47b
Craig Rupp <craigtrupp8@gmail.com>
2023-06-29 13:40:42 -0500

    Initial Commit

---
+++ Python_SectionI/Recursion.py
@@ -0,0 +1,51 @@
+# The function sum_positive_numbers should return the sum of all positive numbers between the number n received and 1. 
+# For example, when n is 3 it should return 1+2+3=6, and when n is 5 it should return 1+2+3+4+5=15.
+
+def sum_positive_numbers(n):
+    # The base case is n being smaller than 1
+    if n <= 1:
+        return n
+    # The recursive case is adding this number to 
+    # the sum of the numbers smaller than this one.
+    return n + sum_positive_numbers(n-1)
+
+print(sum_positive_numbers(3)) # Should be 6
+print(sum_positive_numbers(5)) # Should be 15
+
+## Outputs from above
+# 6
+# 15
+
+## Run for 3 
+# First Iteration : 
+    # 3 < 1 False  so it returns the function call 3 + func_(2) 
+    # Important to remember the 3 stays in memory so we can add on to it later
+# Second Iteration : 
+    # 2 < 1 False so it returns the function call 2 + func(1)
+    # Now we have the numbers 3 (first iteration), 2 (second iteration) and the func call
+# Third Iteration : 
+    # 1 <= 1 
+    # So we lastly return the N (which would be 1)
+# Final (3 + 2  + 1)
+
+
+def factorial(n):
+    print("Factorial called with " + str(n))
+    if n <= 1:
+        print("Returning 1")
+        return 1
+    result = n * factorial(n -1)
+    print("Returning " + str(result) + " for factorial of " + str(n))
+    return result
+
+print(factorial(4))
+
+# Factorial called with 4
+# Factorial called with 3
+# Factorial called with 2
+# Factorial called with 1
+# Returning 1
+# Returning 2 for factorial of 2
+# Returning 6 for factorial of 3
+# Returning 24 for factorial of 4
+# 24
---
+++ Python_SectionI/WhileLoops.py
@@ -0,0 +1,74 @@
+## Power of Two
+def is_power_of_two(number):
+  if number == 0:
+    return False 
+  while number % 2 == 0:
+    number = number / 2
+  return number == 1
+  
+
+# Calls to the function
+print(is_power_of_two(0)) # Should be False
+print(is_power_of_two(1)) # Should be True
+print(is_power_of_two(8)) # Should be True
+print(is_power_of_two(9)) # Should be False
+
+## Result when Running in terminal (See Course Notes for further detail on handling if needed )
+# False
+# True
+# True
+# False
+
+## Sum Divisors : Return the sum of all divisors of a number, without including the number itself 
+# Reminder : Divisor is a number that divides into the number without a remainder
+def sum_divisors(number):
+    divisor = 1
+    total = 0
+    # Offset any values less than 1 to return no divisors (or the total of 0)
+    if number < 1:
+        return 0 
+    # This will execute at least once with the offset above (2 > 1)
+    while number > divisor:
+        # Checks if passed number is divisible by dividsor (1 initially which it will be and checks to make sure to not add to the total if the number is simply divisible by itself (divisor * number))
+        if number % divisor == 0 and number * divisor != number:
+            total += divisor
+            divisor += 1
+        else:
+            divisor += 1
+    return total
+
+print(sum_divisors(0)) # Should print 0
+print(sum_divisors(2)) # Should print 0 not by itself
+print(sum_divisors(3)) # Should print 0 as only by 1 and itself
+print(sum_divisors(6)) # Would think just 5 (2 and 3) Should print 1+2+3+4+6+9+12+18
+print(sum_divisors(36)) # 55
+print(sum_divisors(102)) # 114
+
+# 0
+# 0
+# 0
+# 5
+# 54
+# 113
+
+
+# Variable accepted should be multipled by numbers 1 through 5  and printed with the multiplication table (see print statement)
+# Code needs to limit the result to not exceed 25 
+def multiplication_table(number):
+    # Initialize the appropriate variable
+    multiplier = 1
+
+    # Complete the while loop condition.
+    while multiplier < 6:
+        result = number * multiplier 
+        if  result > 25 :
+            # Enter the action to take if the result is greater than 25
+            return 
+        print(str(number) + "x" + str(multiplier) + "=" + str(result))
+        
+        # Increment the appropriate variable
+        multiplier += 1
+
+print(multiplication_table(3))
+print(multiplication_table(5))
+print(multiplication_table(8))
