c3b73d4b033bc244d44b051db6fba7aaa9a31b1b
Craig Rupp <craigtrupp8@gmail.com>
2023-07-05 12:08:18 -0500

    Classes, OOP Practice, Special Method usage

--- Python_SectionI/Dictionaries.py
+++ Python_SectionI/Dictionaries.py
@@ -221,4 +221,5 @@ def add_prices(basket):
 
 groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59, "coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
 
-print(add_prices(groceries)) 
+print(add_prices(groceries))
+# 28.44 
